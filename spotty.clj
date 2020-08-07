a7deeff24a8e54a0a6ffff537d22afd8ec3de064
Russell Matney <russell.matney@gmail.com>
2020-08-07 18:28:42 -0400

    refactor: remove `sp` dependency
    Now only depends on installing `gtk3`. (And spotify, i suppose).

--- spotty.clj
+++ spotty.clj
@@ -1,11 +1,9 @@
 #!/usr/bin/env bb
 
-;; Dependencies:
-
-;; `sp` bash script, found here:
+;; Spotify dbus usage inspired by `sp` bash script, found here:
 ;; https://gist.github.com/streetturtle/fa6258f3ff7b17747ee3
 
-;; `gtk3` (for `gtk-launch`)
+;; Requires `gtk3` (for `gtk-launch`)
 
 (require '[clojure.string :as string])
 (require '[clojure.java.shell :as sh])
@@ -24,11 +22,21 @@
 
 (comment
   (url->uri "https://open.spotify.com/track/7oaEjLP2dTJLJsITbAxTOz")
-  (url->uri "https://open.spotify.com/track/0oks4FnzhNp5QPTZtoet7c?si=9qdFwTT2Tm-54DNT9wWSbA")
-  )
+  (url->uri "https://open.spotify.com/track/0oks4FnzhNp5QPTZtoet7c?si=9qdFwTT2Tm-54DNT9wWSbA"))
+
+(defn sp-dbus [dbus-cmd arg]
+  (let [sp-dest "org.mpris.MediaPlayer2.spotify"
+        sp-path "/org/mpris/MediaPlayer2"
+        sp-memb "org.mpris.MediaPlayer2.Player"]
+    (sh/sh "dbus-send"
+           "--print-reply"
+           (str "--dest=" sp-dest)
+           sp-path
+           (str sp-memb "." dbus-cmd)
+           arg)))
 
 (defn open-spotify-uri [uri]
-  (sh/sh "sp" "open" uri))
+  (sp-dbus "OpenUri" (str "string:" uri)))
 
 (defn open-other-url
   "Passes the desktop entry and args to `/usr/bin/gtk-launch`.
